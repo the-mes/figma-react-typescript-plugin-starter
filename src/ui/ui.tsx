05727f75276163d5a469b5cd0dddfc823f47fb39
Jakub Biesiada <kbba@op.pl>
2021-04-11 16:29:29 +0200

    Update ui.tsx

--- src/ui/ui.tsx
+++ src/ui/ui.tsx
@@ -1,16 +1,5 @@
-// import type { ExtendedMessageEvent } from '../shared/types/ExtendedMessageEvent';
-
-// import styles from './ui.module.scss';
+import styles from './ui.module.scss';
 
 export const App = () => {
-  // onmessage = (event: ExtendedMessageEvent) => {
-  //   const message = event.data.pluginMessage;
-
-  //   if (message.type === 'get-settings') {
-  //     // TODO
-  //   }
-  // };
-
-  // TODO
   return <main></main>;
 };
